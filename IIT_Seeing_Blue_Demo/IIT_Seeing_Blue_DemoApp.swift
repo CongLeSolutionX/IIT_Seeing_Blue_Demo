72b83d5090d46300dec23296b012170eeae92563
Cong Le <longchike@gmail.com>
2025-06-30 02:00:20 -0700

    The setup

---
+++ IIT_Seeing_Blue_Demo/IIT_Seeing_Blue_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIT_Seeing_Blue_DemoApp.swift
+//  IIT_Seeing_Blue_Demo
+//
+//  Created by Cong Le on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct IIT_Seeing_Blue_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
